532ab8642409f7256738e96ad41454ce0c103741
rohitp4nt <rohitpant90122@gmail.com>
2024-09-17 23:09:30 +0530

    adding responsiveness

--- script.js
+++ script.js
@@ -33,46 +33,46 @@ tl.to("#leaf2",{
 }, 'orange')
 
 
-var tl2 = gsap.timeline({scrollTrigger:{
-    trigger: ".three",
-    start: "20% 95%",
-    end: "20% 50%",
-    scrub: true,
-    // markers: true,
-}})
+// var tl2 = gsap.timeline({scrollTrigger:{
+//     trigger: ".three",
+//     start: "20% 95%",
+//     end: "20% 50%",
+//     scrub: true,
+//     // markers: true,
+// }})
 
-tl2.from(".lemon1",{
-    rotate: "-90deg",
-    left: "-100%",
-    top: "110%"
-}, 'ca')
-tl2.from("#cocacola",{
-    rotate: "360deg",
-    top: "110%",
-    left: "-10%",
-}, 'ca')
+// tl2.from(".lemon1",{
+//     rotate: "-90deg",
+//     left: "-100%",
+//     top: "110%"
+// }, 'ca')
+// tl2.from("#cocacola",{
+//     rotate: "360deg",
+//     top: "110%",
+//     left: "-10%",
+// }, 'ca')
 
-tl2.from(".lemon2",{
-    rotate: "90deg",
-    left: "100%",
-    top: "110%"
-}, 'ca')
-tl2.from("#pepsi",{
-    rotate: "-360deg",
-    top: "110%",
-    left: "100%",
-}, 'ca')
+// tl2.from(".lemon2",{
+//     rotate: "90deg",
+//     left: "100%",
+//     top: "110%"
+// }, 'ca')
+// tl2.from("#pepsi",{
+//     rotate: "-360deg",
+//     top: "110%",
+//     left: "100%",
+// }, 'ca')
 
-tl2.to("#orange-cut",{
-    width:"18%",
-    left: "42%",
-    top: "204%"
-}, 'ca')
-tl2.to("#fanta",{
-    width:"35%",
-    top: "210%",
-    left: "33%",
-}, 'ca')
+// tl2.to("#orange-cut",{
+//     width:"18%",
+//     left: "42%",
+//     top: "204%"
+// }, 'ca')
+// tl2.to("#fanta",{
+//     width:"35%",
+//     top: "210%",
+//     left: "33%",
+// }, 'ca')
 
 
 
